1afd760e207aa9df21614830ab82bb47e9e53152
carlosDevSanchez07 <carlos19972107sanchez@gmail.com>
2023-09-23 11:44:56 -0500

    fix issued manifest in document

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -46,7 +46,7 @@ class MyDocument extends Document<Props> {
           <link rel="apple-touch-icon" sizes="180x180" href="/api/logo?type=apple-touch-icon" />
           <link rel="icon" type="image/png" sizes="32x32" href="/api/logo?type=favicon-32" />
           <link rel="icon" type="image/png" sizes="16x16" href="/api/logo?type=favicon-16" />
-          <link rel="manifest" href="/webmanifest.json" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
           <meta name="msapplication-TileColor" content="#ff0000" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#f9fafb" />
